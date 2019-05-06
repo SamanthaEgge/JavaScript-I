dcc953cba4d8eb32fb3f810671c5f4a4579fa322
unknown <samantha.c.egge@gmail.com>
2019-05-06 15:36:14 -0700

    Completed MVP for arrays.js and function-conversion.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -20,7 +20,8 @@ let anotherFunction = (param) => {
    return param;
  };
 
-anotherFunction("Example");
+
+console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
@@ -31,7 +32,7 @@ let add = (param1, param2) => {
   return param1 + param2;
 };
 
-add(1,2);
+console.log(add(1,2));
 
 
 // let subtract = function (param1, param2) {
@@ -42,7 +43,8 @@ add(1,2);
 let subtract = (param1, param2) => {
   return param1 - param2;
 };
-subtract(1,2);
+
+console.log(subtract(1,2));
 
 
 // Stretch
